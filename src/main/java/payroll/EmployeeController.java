5f579ae060347b10fb9fdab343b5443a8f616ef6
Kirill <atray@mail.ru>
2020-07-21 18:42:56 +0300

    before new put done some shitfull staff

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -53,9 +53,9 @@ public class EmployeeController {
     }
 
     @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee,
+    ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee,
                              @PathVariable Long id){
-        return repository.findById(id)
+        Employee  updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
@@ -65,11 +65,17 @@ public class EmployeeController {
                     newEmployee.setId(id);
                     return repository.save(newEmployee);
                 });
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
 
