046879bbc45f5118c74ff379d2a86b9be8a23da8
kirill <atray@mail.ru>
2020-07-22 16:13:23 +0300

    after exceptions

--- src/main/java/payroll/EmployeeNotFoundException.java
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -4,8 +4,6 @@ class EmployeeNotFoundException extends RuntimeException {
 
     EmployeeNotFoundException(Long id){
         super("Could not find employee " + id);
-        System.out.println("i'm here");
-
     }
 }
 
